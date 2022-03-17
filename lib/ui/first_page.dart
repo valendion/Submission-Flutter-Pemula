f8c05774020055d1ddf7b44544e5c80c6594e6bb
valendion <valendion874@gmail.com>
2022-03-17 15:37:42 +0800

    Fix text field input

--- lib/ui/first_page.dart
+++ lib/ui/first_page.dart
@@ -1,120 +1,110 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_pemula_project/style/theme.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class FirstPage extends StatelessWidget {
   TextEditingController _controller = TextEditingController();
 
-  FirstPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Stack(children: [
-          Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: blueVeryDarkColor,
-                ),
-              ),
-              Expanded(
-                  child: Container(
-                child: Column(),
-                color: whitePrimaryColor,
-              )),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
             children: [
               Container(
-                  margin: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    // boxShadow: [BoxShadow(
-                    //     color: Colors.black54,
-                    //     blurRadius: 15.0,
-                    //     offset: Offset(0.0, 0.75)
-                    // )]
-                  ),
-                  child: SvgPicture.asset(
-                    'assets/images/illustrator_doctor.svg',
-                    width: 400,
-                  )),
-              Text(
-                'Enjoy your health anywhere',
-                style: textTItleDark,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'You can find out information about health\n and ask the experts directly',
-                style: textDesc2,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              // Container(
-              //   margin: EdgeInsets.all(16),
-              //   child: TextField(
-              //     decoration: InputDecoration(
-              //         hintText: 'Please Input yout name ...',
-              //         labelText: 'Your name'),
-              //     controller: _controller,
-              //   ),
-              // ),
-              Container(
-                padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                margin: const EdgeInsets.all(16),
-                child: TextFormField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    labelText: 'Enter your name',
-                    labelStyle: textDesc3,
-                    border: const OutlineInputBorder(),
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: const EdgeInsets.all(16),
-                padding: const EdgeInsets.only(
-                  left: 16,
-                  right: 4,
-                ),
-                decoration: BoxDecoration(
-                    color: bluePrimaryColor,
-                    borderRadius: BorderRadius.all(const Radius.circular(40))),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
+                    Container(
+                        margin: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/images/illustrator_doctor.svg',
+                          width: 400,
+                        )),
+                  ],
+                ),
+                color: blueVeryDarkColor,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+
                     Text(
-                      'Get Started',
-                      style: textButton,
+                      'Enjoy your health anywhere',
+                      style: textTItleDark,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'You can find out information about health\n and ask the experts directly',
+                      style: textDesc2,
+                      textAlign: TextAlign.center,
                     ),
                     const SizedBox(
-                      width: 8,
+                      height: 20,
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.arrow_forward_outlined),
-                      color: whitePrimaryColor,
+                    Container(
+                      margin: EdgeInsets.all(16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter your name',
+                          labelStyle: textDesc3,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(16),
+                      padding: EdgeInsets.only(
+                        left: 16,
+                        right: 4,
+                      ),
+                      decoration: BoxDecoration(
+                          color: bluePrimaryColor,
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Get Started',
+                            style: textButton,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          IconButton(
+                            onPressed: () {
+
+                            },
+                            icon: const Icon(Icons.arrow_forward_outlined),
+                            color: whitePrimaryColor,
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
-              )
+                color: whitePrimaryColor,
+              ),
             ],
-          )
-        ]),
+          ),
+        ),
       ),
     );
   }
